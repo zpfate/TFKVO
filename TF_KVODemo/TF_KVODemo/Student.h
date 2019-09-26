d318f1f12f894857e0b459a2617c06038da503b7
ZpFate <zptwistedfate@163.com>
2019-09-26 16:52:21 +0800

    完成自己实现的KVO

---
+++ TF_KVODemo/TF_KVODemo/Student.h
@@ -0,0 +1,17 @@
+//
+//  Student.h
+//  TF_KVODemo
+//
+//  Created by Twisted Fate on 2019/9/26.
+//  Copyright © 2019 TwistedFate. All rights reserved.
+//
+
+#import "Person.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : Person
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TF_KVODemo/TF_KVODemo/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  TF_KVODemo
+//
+//  Created by Twisted Fate on 2019/9/26.
+//  Copyright © 2019 TwistedFate. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
